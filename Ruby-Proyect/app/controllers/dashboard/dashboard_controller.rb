9b8bcafb2ceb1e59968dabbbb779a2894c8b7795
JuanCaballero09 <juanes0921200@gmail.com>
2025-08-16 14:16:57 -0500

    pausa

--- Ruby-Proyect/app/controllers/dashboard/dashboard_controller.rb
+++ Ruby-Proyect/app/controllers/dashboard/dashboard_controller.rb
@@ -2,7 +2,7 @@ class Dashboard::DashboardController < ApplicationController
   layout "dashboard"
 
   before_action :authenticate_user!
-  before_action :check_admin
+  before_action :check_access # Verifica si el usuario tiene acceso a la sección del dashboard
 
 
   def index
@@ -17,9 +17,20 @@ class Dashboard::DashboardController < ApplicationController
     end
   end
 
-  def load_charts_data
-    @products_by_group = Product.joins(:grupo).group('grupos.nombre').count
-    @grupos_growth = Grupo.group_by_month(:created_at, format: "%b %Y").count
+  def check_employee
+    unless current_user.empleado? # Verifica si el usuario tiene rol empleado
+      redirect_to root_path, alert: "No tienes acceso a esta página." # Redirige si no es empleado
+    end
   end
 
+  def check_access
+    return if current_user.admin? || current_user.empleado? # Permite acceso si es admin o empleado
+
+    redirect_to root_path, alert: "No tienes acceso a esta página." # Redirige si no es admin ni empleado
+  end
+
+  def load_charts_data
+    @products_by_group = Product.joins(:grupo).group("grupos.nombre").count
+    @grupos_growth = Grupo.group_by_month(:created_at, format: "%b %Y").count
+  end
 end
