099e25039863db183d904c0f123918347d9e21c4
sanzambra <166321055+San5472@users.noreply.github.com>
2025-10-10 14:46:07 -0500

    Feature/289 (#292)

--- Ruby-Proyect/app/mailers/application_mailer.rb
+++ Ruby-Proyect/app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "santiagozambrano751@gmail.com" # Esto es de donde van a salir los correos hacia las personas que olviden su contraseña
+  default from: "biteviasoftware@gmail.com" # Esto es de donde van a salir los correos hacia las personas que olviden su contraseña
   layout "mailer"
 end
